a74cd9106a0e2691ae8cb326d78de6a5283180d5
Kuyho Chung <kchung1995@gmail.com>
2023-08-05 03:58:44 +0900

    feat: changed directory name to _queue

--- newblogpost.sh
+++ newblogpost.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 # Step 1: Check if 'queue' folder exists and is not empty
-if [ -d "queue" ] && [ -n "$(ls -A queue)" ]; then
+if [ -d "_queue" ] && [ -n "$(ls -A _queue)" ]; then
   # Step 2: Move markdown files to 'posts' folder and rename them
-  for file in queue/*.md; do
+  for file in _queue/*.md; do
     if [ -f "$file" ]; then
       FILE_NAME=$(basename "$file" .md)
       NEW_FILENAME=$(date +%Y-%m-%d-"$FILE_NAME".md)
